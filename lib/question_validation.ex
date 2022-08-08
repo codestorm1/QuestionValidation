d3e79cc750cd4e70bf870ab9c4fc6e7ffd2b1be1
Bryan Green <twistedogre@gmail.com>
2022-08-07 23:25:55 -0700

    Added some comments

--- lib/question_validation.ex
+++ lib/question_validation.ex
@@ -3,38 +3,49 @@ defmodule QuestionValidation do
   Documentation for `QuestionValidation`.
   """
 
+  @spec validate(map(), map()) :: map()
+  @doc """
+  This function takes a map of questions, and a map of answers.
+  It returns a map of questions and errors.
+  If there are no errors, it returns an empty map.
+  """
   def validate(question_map, answer_map) do
     answers = map_keys_to_strings(answer_map)
 
-    {_terminal_chosen, errors} =
+    {_completion_chosen, errors} =
       question_map
       |> Enum.with_index()
-      |> Enum.reduce({false, %{}}, fn {question, index}, {acc_terminal_chosen, acc_error_map} ->
-        {option_count, terminal_option} = get_option_info({question, index})
+      |> Enum.reduce({false, %{}}, fn {question, index}, {acc_completion_chosen, acc_error_map} ->
+        {option_count, completion_option} = get_option_info({question, index})
         {key, answer} = get_answer(answers, index)
-        error_map = Map.merge(acc_error_map, get_any_errors(key, answer, option_count, acc_terminal_chosen))
-        terminal_chosen = acc_terminal_chosen or terminal_chosen(terminal_option, answer)
-        {terminal_chosen, error_map}
+        error_map = Map.merge(acc_error_map, get_any_errors(key, answer, option_count, acc_completion_chosen))
+        completion_chosen = acc_completion_chosen or completion_chosen(completion_option, answer)
+        {completion_chosen, error_map}
       end)
 
     errors
   end
 
   defp get_answer(answers, question_index) do
+    # lookup question by question number, i.e. "q0"
     key = "q#{question_index}"
     answer = Map.get(answers, key)
     {key, answer}
   end
 
-  defp terminal_chosen(terminal_option, answer) when is_number(terminal_option) and is_number(answer) do
-    answer == terminal_option
+  defp completion_chosen(completion_option, answer) when is_number(completion_option) and is_number(answer) do
+    # if there is a completion_option that matches the given answer, the completion option was chosen
+    answer == completion_option
   end
 
-  defp terminal_chosen(_terminal_option, _answer) do
+  defp completion_chosen(_completion_option, _answer) do
+    # missing completion option and/or answer
     false
   end
 
-  defp get_any_errors(key, answer, option_count, _terminal_chosen = false) do
+  defp get_any_errors(key, answer, option_count, _completion_chosen = false) do
+    # No completion answer has been chosen yet.
+    # If the answer is missing or invalid, return the error in a map, otherwise an empty map
     cond do
       answer == nil ->
         %{key => "was not answered"}
@@ -47,26 +58,30 @@ defmodule QuestionValidation do
     end
   end
 
-  defp get_any_errors(key, answer, _option_count, _terminal_chosen = true) do
+  defp get_any_errors(key, answer, _option_count, _completion_chosen = true) do
+    # completion answer was already chosen, any more answers are invalid
     cond do
       answer != nil ->
         %{key => "was answered even though a previous response indicated that the questions were complete"}
+
       true ->
         %{}
     end
   end
 
   defp get_option_info(question_tuple) do
+    # get the number of options and which option, if any, signals completion
     options =
       question_tuple
       |> elem(0)
       |> Map.get(:options)
 
-    {length(options), get_terminal_option(options)}
+    {length(options), get_completion_option(options)}
   end
 
-  defp get_terminal_option(options) do
-    terminal_option =
+  defp get_completion_option(options) do
+    # Finds a single completion option in the list, if one exists
+    completion_option =
       options
       |> Enum.with_index()
       |> Enum.filter(fn {option, _index} ->
@@ -74,14 +89,16 @@ defmodule QuestionValidation do
       end)
       |> List.first()
 
-    unless terminal_option == nil do
-      {_option, option_num} = terminal_option
+    unless completion_option == nil do
+      {_option, option_num} = completion_option
       option_num
     else
       nil
     end
   end
 
+  # These are used to turn the Ruby style hash data to dictionaries
+  # The tests could be changed to pass Elixir style maps instead
   defp map_keys_to_strings(nil), do: %{}
   defp map_keys_to_strings({}), do: %{}
 
